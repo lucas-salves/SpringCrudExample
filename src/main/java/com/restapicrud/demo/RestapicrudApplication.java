35f59bfeb07a1f82a7e1d293fc8e40a4995ba77c
Lucas Sousa Alves <lucas.salves@terra.com.br>
2021-08-17 20:20:54 -0300

    initial commit

---
+++ src/main/java/com/restapicrud/demo/RestapicrudApplication.java
@@ -0,0 +1,13 @@
+package com.restapicrud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapicrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapicrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapicrud/demo/RestapicrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapicrud.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapicrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
